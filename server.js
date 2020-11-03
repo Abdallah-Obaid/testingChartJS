1fe0cadd613da8d760fbcae7e56205f0901981be
msallal <m.sallal@penguinin.com>
2020-11-03 14:54:55 +0200

    deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 // Load environment variables from .env
 require('dotenv').config();
-const client = require('./DBconection.js');
+// const client = require('./DBconection.js');
 // Application dependencies
 const express = require('express');
 const cors = require('cors');
@@ -19,15 +19,15 @@ server.use(express.static('./public'));
 //     response.send('IT WORK');
 // });
 server.get('/', (req,res)=>{
-  client.query('select * from users')
-    .then((results)=> {console.table(results.rows);var names=[];var attend=[]; results.rows.forEach((ele)=>{names.push(ele.first_name);attend.push(ele.attendance);});   res.render('./index',{names:names,attend:attend });
-    });});
+  // client.query('select * from users')
+  res.render('./index',{names:'names',attend:'attend' });
+});
 
-client.connect()
-  .then(()=>{
-    server.listen(PORT, () => console.log(`App is listening on ${PORT}`,Date()));
-  })
-  .catch(e => {console.log(e);});
+// client.connect()
+// .then(()=>{
+server.listen(PORT, () => console.log(`App is listening on ${PORT}`,Date()));
+// })
+// .catch(e => {console.log(e);});
 // .then(()=> console.log('Connected Successfully'))
 // .catch(e=>console.log('hi'));
 
